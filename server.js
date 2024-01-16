d08e0d2435af344344409f58bc4394172e8b78d3
Von Souza <von.souza@impactzero.pt>
2024-01-16 20:05:21 -0300

    Connect node project to MongoDB

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express');
+
+const mongodb = require('./data/database');
+
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(`/`, require(`./routes`));
+app.use('/', require('./routes'));
+
+
+mongodb.initDb((err) => {
+    if(err){
+        console.log(err);
+    }
+    else{
+        app.listen(port, () => {console.log(`Database is listening and node Running on port ${port}`)});
+    }
+})
 
 app.listen(port, () => {console.log(`Runing on port ${port}`)});
